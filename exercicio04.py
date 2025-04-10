b338586e2ff9bd7bb6fae9045ac255099c8c6ed4
thomthomtt <thomazgt39@gmail.com>
2025-04-10 15:46:00 -0300

    contador de numero negatibos

--- exercicio04.py
+++ exercicio04.py
@@ -1,6 +1,6 @@
 num = int(input("Digite um número: "))
-for i in range(1, num):
-    if num <= 0:
-        print("O número digitado não é valido")
-    else:
+if num <= 0:
+    print("O número digitado não é valido")
+else:
+    for i in range(1, num):
         print(i)
---
+++ exercicio05.py
@@ -0,0 +1,8 @@
+neg=0
+for i in range(10):
+    num=int(input("Digite um número: "))
+    if num<0:
+        neg+=1
+print(neg)
+
+
